a09e1afe1fee230381e96296dac8ebd5f00a595e
giorgi <giorgi.muqeria@gmail.com>
2023-08-28 11:49:08 +0400

    add CreatedAt attribute in comment model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\SetAuthorTrait;
 
@@ -20,6 +22,13 @@ class Comment extends Model
     public function user()
     {
         return $this->belongsTo(User::class,'user_id','id');
+    }
+
+    protected function CreatedAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => Carbon::parse($value)->format('F d, Y H:m')
+        );
     }
 
 }
